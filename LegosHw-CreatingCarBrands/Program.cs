f38ad1ad8122df78467c620cdf2a92a1ccc2bd28
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-04-16 20:23:05 -0500

    test

--- LegosHw-CreatingCarBrands/Program.cs
+++ LegosHw-CreatingCarBrands/Program.cs
@@ -10,7 +10,7 @@ namespace LegosHw_CreatingCarBrands
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Hello, world!!");
         }
     }
 }
